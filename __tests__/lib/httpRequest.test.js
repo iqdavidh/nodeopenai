afcfd9cbaf610d705b0e69c98f1dec76b5f94439
davidh <iq.davidh@gmail.com>
2024-06-20 21:24:43 -0600

    Squashed commit of the following:
    commit cbb2fb9bfd57fa5dd671d33d9a221c2d14d9bdd5
    Author: davidh <iq.davidh@gmail.com>
    Date:   Thu Jun 20 21:22:25 2024 -0600
    
        initial
    
    commit 39c0a34e945391c72d13d270d6dba6a7dd599385
    Author: davidh <iq.davidh@gmail.com>
    Date:   Thu Jun 20 21:03:27 2024 -0600
    
        initial
    
    commit 1ba178891c54693108b3ac7acdc9e3608478c119
    Author: davidh <iq.davidh@gmail.com>
    Date:   Wed Jun 19 15:47:39 2024 -0600
    
        initial
    
    commit 76a3216aab97dd97660d17629f5ebd5f7548e988
    Author: davidh <iq.davidh@gmail.com>
    Date:   Wed Jun 19 15:14:18 2024 -0600
    
        initial
    
    commit 57f4f32490699dfe8cddadcca004c64b56a41011
    Author: davidh <iq.davidh@gmail.com>
    Date:   Fri Jun 14 15:57:59 2024 -0600
    
        initial

--- __tests__/lib/httpRequest.test.js
+++
@@ -1,9 +0,0 @@
-const httpRequest = require("../../src/lib/httpRequest");
-describe("httpRequest", () => {
-  it("get request",async () => {
-    const url = "https://dummyapi.online/api/movies"
-    const response = await httpRequest.GET(url);
-    expect(response.success).toBe(true);
-    expect(Array.isArray(response.rawResponse)).toBe(true)
-  });
-});
